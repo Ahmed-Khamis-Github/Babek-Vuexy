57de95be1425a92359b007960c1e970b5cf70000
Ahmed Khamis <miso201333@gmail.comm>
2024-03-06 08:45:08 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\language\LanguageController;
+use App\Http\Controllers\pages\HomePage;
+use App\Http\Controllers\pages\Page2;
+use App\Http\Controllers\pages\MiscError;
+use App\Http\Controllers\authentications\LoginBasic;
+use App\Http\Controllers\authentications\RegisterBasic;
+use App\Http\Controllers\Dashboard\CompanyController;
+use App\Http\Controllers\Dashboard\OrderController;
+use App\Http\Controllers\Dashboard\OrderUserController;
+use App\Http\Controllers\Dashboard\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Main Page Route
+Route::get('/page-2', [Page2::class, 'index'])->name('pages-page-2');
+
+// locale
+Route::get('lang/{locale}', [LanguageController::class, 'swap']);
+
+// pages
+Route::get('/pages/misc-error', [MiscError::class, 'index'])->name('pages-misc-error');
+
+
+Route::middleware(['auth'])->group(function () {
+    // Landing page route
+    Route::get('/', [HomePage::class, 'index'])->name('pages-home');
+
+    // Users resource routes
+    Route::resource('users', UserController::class);
+
+    // Companies resource routes
+    Route::resource('companies', CompanyController::class);
+
+    //offers 
+    Route::get('orders/with-offers', [OrderUserController::class, 'showOrdersWithOffers'])->name('offers.show_orders_with_offers');
+    Route::get('orders/{order}/offers', [OrderUserController::class, 'showOffersForOrder'])->name('offers.show_for_order');
+
+    Route::patch('offers/accept/{offer}', [OrderUserController::class, 'acceptOffer'])->name('offers.accept');
+
+    // orders routes 
+    Route::resource('orders', OrderController::class);
+});
+
+
+//auth routes 
+Route::get('login', [LoginBasic::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginBasic::class, 'authenticate']);
+Route::post('logout', [LoginBasic::class, 'logout'])->name('logout');
+
+
+// Route::get('orders/{order}/applied-offers', [OrderUserController::class, 'showAppliedOffers'])->name('orders.applied_offers');
